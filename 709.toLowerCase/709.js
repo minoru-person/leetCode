00642e8bc614333f0f5b4ceb286ebbbfb26dc62b
minoru nishino <20012166+tenoctillion@users.noreply.github.com>
2019-01-30 13:30:18 +0800

    Update 709.js
    Shorten code. Remove extra step.

--- 709.toLowerCase/709.js
+++ 709.toLowerCase/709.js
@@ -1,14 +1,14 @@
 var toLowerCaseMy = function(str){
-    var myChar;
+    //var myChar;
     var myStr;
     var myArray =[];
     
     for(var i=0; i<str.length; i++){
     
 	     if(64<str.charCodeAt(i) && str.charCodeAt(i)<91){
-            myChar = str.charCodeAt(i);
-	          myChar = myChar + 32;
-	          myStr = String.fromCharCode(myChar);
+                  //myChar = str.charCodeAt(i);
+	          //myChar = myChar + 32;
+	          myStr = String.fromCharCode(str.charCodeAt(i));
 	    	    myArray.push(myStr);
 	     } else {
 	          myArray.push(str.charAt(i));
