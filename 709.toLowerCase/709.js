af12247758b6f38ecfc50fd286fc2d483ca5b69b
minoru nishino <20012166+tenoctillion@users.noreply.github.com>
2019-01-30 12:40:10 +0800

    Create 709.js

---
+++ 709.toLowerCase/709.js
@@ -0,0 +1,21 @@
+var toLowerCaseMy = function(str){
+    var myChar;
+    var myStr;
+    var myArray =[];
+    
+    for(var i=0; i<str.length; i++){
+    
+	     if(64<str.charCodeAt(i) && str.charCodeAt(i)<91){
+            myChar = str.charCodeAt(i);
+	          myChar = myChar + 32;
+	          myStr = String.fromCharCode(myChar);
+	    	    myArray.push(myStr);
+	     } else {
+	          myArray.push(str.charAt(i));
+	     }
+       
+    }
+    
+    myArray = myArray.join('');
+    return myArray;
+};
